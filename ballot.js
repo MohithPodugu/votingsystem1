b88c5bf601688a463b2b9717d29e221164a5322c
MohithPodugu <149921625+MohithPodugu@users.noreply.github.com>
2024-03-10 10:25:18 +0530

    Add files via upload

---
+++ ballot.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Ballot extends React.Component
+{
+    render()
+    {
+        return(
+            <div className="fullheight">
+                <h1>This is ballot</h1>
+            </div>
+        );
+    }
+}
+
+export default Ballot;
